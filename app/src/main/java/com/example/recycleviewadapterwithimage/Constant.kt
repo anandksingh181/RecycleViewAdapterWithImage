e2465530d57d975515c241623da04a6a05b9e274
anandksingh181 <142203756+anandksingh181@users.noreply.github.com>
2024-02-02 17:31:31 +0530

    Constant

--- app/src/main/java/com/example/recycleviewadapterwithimage/Constant.kt
+++ app/src/main/java/com/example/recycleviewadapterwithimage/Constant.kt
@@ -6,27 +6,10 @@ object Constant {
     fun getData():ArrayList<RvModel>{
         dataList = ArrayList<RvModel>()
 
-        dataList.add(RvModel(R.drawable.anand,"Anand Kumar Singh","9129757212","Sector-71"))
-        dataList.add(RvModel(R.drawable.anand,"Anand Kumar Singh","9129757212","Sector-71"))
-        dataList.add(RvModel(R.drawable.anand,"Anand Kumar Singh","9129757212","Sector-71"))
-        dataList.add(RvModel(R.drawable.anand,"Anand Kumar Singh","9129757212","Sector-71"))
-        dataList.add(RvModel(R.drawable.anand,"Anand Kumar Singh","9129757212","Sector-71"))
-        dataList.add(RvModel(R.drawable.anand,"Anand Kumar Singh","9129757212","Sector-71"))
-        dataList.add(RvModel(R.drawable.anand,"Anand Kumar Singh","9129757212","Sector-71"))
-        dataList.add(RvModel(R.drawable.anand,"Anand Kumar Singh","9129757212","Sector-71"))
-        dataList.add(RvModel(R.drawable.anand,"Anand Kumar Singh","9129757212","Sector-71"))
-        dataList.add(RvModel(R.drawable.anand,"Anand Kumar Singh","9129757212","Sector-71"))
-        dataList.add(RvModel(R.drawable.anand,"Anand Kumar Singh","9129757212","Sector-71"))
-        dataList.add(RvModel(R.drawable.anand,"Anand Kumar Singh","9129757212","Sector-71"))
-        dataList.add(RvModel(R.drawable.anand,"Anand Kumar Singh","9129757212","Sector-71"))
-        dataList.add(RvModel(R.drawable.anand,"Anand Kumar Singh","9129757212","Sector-71"))
-        dataList.add(RvModel(R.drawable.anand,"Anand Kumar Singh","9129757212","Sector-71"))
-        dataList.add(RvModel(R.drawable.anand,"Anand Kumar Singh","9129757212","Sector-71"))
-        dataList.add(RvModel(R.drawable.anand,"Anand Kumar Singh","9129757212","Sector-71"))
-        dataList.add(RvModel(R.drawable.anand,"Anand Kumar Singh","9129757212","Sector-71"))
-        dataList.add(RvModel(R.drawable.anand,"Anand Kumar Singh","9129757212","Sector-71"))
-        dataList.add(RvModel(R.drawable.anand,"Anand Kumar Singh","9129757212","Sector-71"))
-        dataList.add(RvModel(R.drawable.anand,"Anand Kumar Singh","9129757212","Sector-71"))
+
+        for (i in 1..101) {
+            dataList.add(RvModel(R.drawable.anand, "Anand Kumar Singh", "9129757212", "Sector-71"))
+        }
         return dataList
     }
 
